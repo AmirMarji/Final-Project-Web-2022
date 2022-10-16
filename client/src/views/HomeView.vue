1315ee8c779c8b4b2d19bcfe86772a7392c00ee9
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2022-10-16 15:18:34 -0400

    break

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -3,6 +3,8 @@
 
 <template>
   <main>
-    <h1>Hello</h1>
+    <h1>Welcome</h1>
+    <p>this is the temporary landing page for my exersize application</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error nihil inventore voluptatibus quam porro sequi! Tempora repudiandae rem dignissimos eaque, cupiditate facere autem voluptatibus earum cumque, minima sed odit.</p>
   </main>
 </template>
