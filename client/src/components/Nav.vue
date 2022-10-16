32ecd10d77715b46a1fc0411f00c21ca5e0f7b45
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2022-10-16 16:39:12 -0400

    bulma nav changes without JS

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,65 +1,113 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+let isActive = ref(false);
+</script>
+
 <template>
   <nav class="navbar is-info
 " role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <router-link to="/" class="navbar-item">
+          Home
+        </router-link>
+
+        <router-link to="/workout" class="navbar-item">
+          Workout
+        </router-link>
+        <router-link to="/stats" class="navbar-item">
+          Stats
+        </router-link>
+
+
+        <!-- NOT IMIDIATLY NEEDED BUT IF I DO DECIDE TO DO DROPDOW ITS THERE -->
+
+        <!-- <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              About
+            </a>
+            <a class="navbar-item">
+              Jobs
+            </a>
+            <a class="navbar-item">
+              Contact
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              Report an issue
+            </a>
+          </div>
+        </div> -->
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <!-- start of user login dropdown -->
+            <div class="dropdown">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+      <span>User Log in</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
   </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
+  <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+    <div class="dropdown-content">
+      <a href="#" class="dropdown-item">
+        Jimmy McGill
       </a>
-
-      <a class="navbar-item">
-        Documentation
+      <a href="#" class="dropdown-item">
+        Kim Wexler
       </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
+      <a href="#" class="dropdown-item">
+        Mike Ehrmantraut
+      </a>
+      <a href="#" class="dropdown-item">
+        Howard Hamlin
+      </a>
+      <a href="#" class="dropdown-item">
+        Nacho Varga
+      </a>
+      <a href="#" class="dropdown-item">
+        Gus Fring
+      </a>
+      <a href="#" class="dropdown-item">
+        Chuck McGill
+      </a>
+      <a href="#" class="dropdown-item">
+        Lalo Salamanca
+      </a>
+    </div>
+  </div>
+</div>
+            <router-link to="/admin" class="button is-dark">
+          Admin
+        </router-link>
+          </div>
         </div>
       </div>
     </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-dark">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-dark">
-            Log in
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script setup>
