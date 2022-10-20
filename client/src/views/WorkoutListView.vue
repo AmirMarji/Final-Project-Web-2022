a3d3cee9b9c04e8f90b5c0837b6c2d7739d33908
Amir E Marji <98343618+AmirMarji@users.noreply.github.com>
2022-10-20 15:53:56 -0400

    test

--- client/src/views/WorkoutListView.vue
+++ client/src/views/WorkoutListView.vue
@@ -30,7 +30,7 @@
                             <div class="media">
                                 <div class="media-left">
                                     <figure class="image is-48x48">
-                                        <img src="https://bulma.io/images/placeholders/96x96.png"
+                                        <img v-bind:src="session.user.image"
                                             alt="Placeholder image">
                                     </figure>
                                 </div>
